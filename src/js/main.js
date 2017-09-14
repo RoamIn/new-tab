4100be1b1bd9af998f5759070ec5a1a1d152d571
Pulsating <ivill@live.com>
2017-09-14 23:15:30 +0800

    save, add canvas

---
+++ src/js/main.js
@@ -0,0 +1,32 @@
+const canvas = document.getElementById('J_Canvas');
+const context = canvas.getContext('2d');
+
+function getWindowWidth(){
+    return  'innerWidth' in window ? window.innerWidth : document.documentElement.offsetWidth;
+}
+
+function getWindowHeight(){
+    return  'innerHeight' in window ? window.innerHeight : document.documentElement.offsetHeight;
+}
+
+function updateCanvasSize() {
+    canvas.width = getWindowWidth();
+    canvas.height = getWindowHeight();
+}
+
+function addHandler(element, method, normal){
+    element.addEventListener(normal, method, false); // IE9, Chrome, Safari, Oper
+}
+
+function addResizeHandler(){
+    addHandler(window, resizeHandler, 'resize');
+}
+
+function resizeHandler(){
+    setTimeout(function(){
+        updateCanvasSize();
+    }, 350);
+}
+
+addResizeHandler();
+updateCanvasSize();
